d38ed1befb0c43a7a837db886086b310dfd65068
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-09 17:42:48 -0400

    completed EX 2 pg.46 both options

---
+++ NorthwindTraders/src/main/java/com/pluralsight/Option_1.java
@@ -0,0 +1,48 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Option_1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String connectionString = "jdbc:mysql://localhost:3306/northwind";
+        String username = "user_1";
+        String password = "password1234";
+
+        // Load the MySQL Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Open a connection to the database
+        Connection connection = DriverManager.getConnection(connectionString, username, password);
+
+        // Create a statement
+        Statement statement = connection.createStatement();
+
+        // Define query ProductID, ProductName, UnitPrice, UnitsInStock
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
+
+        // Execute the query
+        ResultSet results = statement.executeQuery(query);
+
+        // Process the results with stacked output format
+        while (results.next()) {
+            int productId = results.getInt("ProductID");
+            String productName = results.getString("ProductName");
+            double unitPrice = results.getDouble("UnitPrice");
+            int unitsInStock = results.getInt("UnitsInStock");
+
+            // Display Option 1 format
+            System.out.println("Product Id: " + productId);
+            System.out.println("Name:");
+            System.out.println(productName);
+            System.out.println("Price:");
+            System.out.printf("%.2f%n", unitPrice);
+            System.out.println("Stock:");
+            System.out.println(unitsInStock);
+            System.out.println("------------------");
+        }
+
+        // Close the connection
+        connection.close();
+    }
+}
