1f169db47a87430e226e3ad7b2bce3f29a739b00
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-10 09:38:11 -0400

    updated way to do sql to java

--- Demo2/src/main/java/com/pluralsight/Main.java
+++ Demo2/src/main/java/com/pluralsight/Main.java
@@ -1,40 +1,95 @@
 package com.pluralsight;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.sql.*;
+import javax.sql.DataSource;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        String connectionString = "jdbc:mysql://localhost:3306/world";
-        String username = "user_1";
-        String password = "password1234";
+    private static sqlConnectionInfo sqlConnectionInfo;
+
+    public static void main(String[] args) {
+
+        if (args.length != 3) {
+            System.out.println(
+                    "Application needs three arguments to run: " +
+                            "java com.pluralsight.Main <username> <password> <sqlUrl>");
+            System.exit(1);
+        }
+
+        sqlConnectionInfo = getSqlConnectionInfoFromArgs(args);
+
+        try {
+            displayCities(103);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static sqlConnectionInfo getSqlConnectionInfoFromArgs(String[] args) {
+        // get the username and password from the command line args
+        String username = args[0];
+        String password = args[1];
+
+        String connectionString = args[2];
+
+        return new sqlConnectionInfo(connectionString, username, password);
+    }
 
 
-        // load the MySQL Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public static void displayCities(int countryId) throws SQLException {
+
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet results = null;
+
+        try {
+
+            // load the MySQL Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
 // 1. open a connection to the database
 // use the database URL to point to the correct database
-        Connection connection;
-        connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/world",
-                username,
-                password);
-// create statement
-// the statement is tied to the open connection
-        Statement statement = connection.createStatement();
-// define your query
-        String query = "SELECT Name FROM city " +
-                "WHERE CountryCode = 'USA'";
-// 2. Execute your query
-        ResultSet results = statement.executeQuery(query);
-// process the results
-        while (results.next()) {
-            String city = results.getString("Name");
-            System.out.println(city);
-        }
-// 3. Close the connection
-        connection.close();
 
+
+            connection = DriverManager.getConnection(
+                    sqlConnectionInfo.getConnectionString(),
+                    sqlConnectionInfo.getUsername(),
+                    sqlConnectionInfo.getPassword());
+
+
+            // define your query
+            String query = "SELECT city FROM city " +
+                    "WHERE country_id = ?";
+
+            // create statement
+// the statement is tied to the open connection
+
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, countryId);
+
+
+// 2. Execute your query
+            results = ps.executeQuery();
+
+// process the results
+            while (results.next()) {
+                String city = results.getString("city");
+                System.out.println(city);
+            }
+// 3. Close the connection
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            if (results != null) results.close();
+            if (ps != null) ps.close();
+            if (connection != null) connection.close();
+
+
+        }
     }
 }
