eba6834f2518990433358fd424c0e67c78a59ceb
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-06-10 10:52:21 -0400

    completed EX 2 pg.53 both options

--- NorthwindTraders/src/main/java/com/pluralsight/Option_1.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Option_1.java
@@ -1,48 +1,93 @@
 package com.pluralsight;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Option_1 {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        String connectionString = "jdbc:mysql://localhost:3306/northwind";
-        String username = "user_1";
-        String password = "password1234";
+    private static sqlConnectionInfo sqlConnectionInfo;
 
-        // Load the MySQL Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    public static void main(String[] args) {
 
-        // Open a connection to the database
-        Connection connection = DriverManager.getConnection(connectionString, username, password);
-
-        // Create a statement
-        Statement statement = connection.createStatement();
-
-        // Define query ProductID, ProductName, UnitPrice, UnitsInStock
-        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products";
-
-        // Execute the query
-        ResultSet results = statement.executeQuery(query);
-
-        // Process the results with stacked output format
-        while (results.next()) {
-            int productId = results.getInt("ProductID");
-            String productName = results.getString("ProductName");
-            double unitPrice = results.getDouble("UnitPrice");
-            int unitsInStock = results.getInt("UnitsInStock");
-
-            // Display Option 1 format
-            System.out.println("Product Id: " + productId);
-            System.out.println("Name:");
-            System.out.println(productName);
-            System.out.println("Price:");
-            System.out.printf("%.2f%n", unitPrice);
-            System.out.println("Stock:");
-            System.out.println(unitsInStock);
-            System.out.println("------------------");
+        // Ensure three arguments are provided
+        if (args.length != 3) {
+            System.out.println("Application needs three arguments to run: " +
+                    "java com.pluralsight.Main <username> <password> <sqlUrl>");
+            System.exit(1);
         }
 
-        // Close the connection
-        connection.close();
+        sqlConnectionInfo = getSqlConnectionInfoFromArgs(args);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                // Display menu
+                System.out.println("What do you want to do?");
+                System.out.println("1) Display all products");
+                System.out.println("2) Display all customers");
+                System.out.println("0) Exit");
+                System.out.print("Select an option: ");
+                int choice = scanner.nextInt();
+
+                // Handle user selection
+                if (choice == 0) {
+                    System.out.println("Exiting...");
+                    break;
+                } else if (choice == 1) {
+                    displayProducts();
+                } else if (choice == 2) {
+                    displayCustomers();
+                } else {
+                    System.out.println("Invalid selection. Try again.");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static sqlConnectionInfo getSqlConnectionInfoFromArgs(String[] args) {
+        return new sqlConnectionInfo(args[2], args[0], args[1]);
+    }
+
+    public static void displayProducts() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(
+                sqlConnectionInfo.getConnectionString(),
+                sqlConnectionInfo.getUsername(),
+                sqlConnectionInfo.getPassword());
+             Statement statement = connection.createStatement();
+             ResultSet results = statement.executeQuery("SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products")) {
+
+            //Option 1
+            while (results.next()) {
+                System.out.println("Product Id: " + results.getInt("ProductID"));
+                System.out.println("Name:");
+                System.out.println(results.getString("ProductName"));
+                System.out.println("Price:");
+                System.out.printf("%.2f%n", results.getDouble("UnitPrice"));
+                System.out.println("Stock:");
+                System.out.println(results.getInt("UnitsInStock"));
+                System.out.println("------------------");
+            }
+        }
+    }
+
+    public static void displayCustomers() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(
+                sqlConnectionInfo.getConnectionString(),
+                sqlConnectionInfo.getUsername(),
+                sqlConnectionInfo.getPassword());
+             Statement statement = connection.createStatement();
+             ResultSet results = statement.executeQuery(
+                     "SELECT ContactName, CompanyName, City, Country, Phone FROM Customers ORDER BY Country")) {
+
+            while (results.next()) {
+                System.out.println("Contact Name: " + results.getString("ContactName"));
+                System.out.println("Company Name: " + results.getString("CompanyName"));
+                System.out.println("City: " + results.getString("City"));
+                System.out.println("Country: " + results.getString("Country"));
+                System.out.println("Phone: " + results.getString("Phone"));
+                System.out.println("------------------");
+            }
+        }
     }
 }
